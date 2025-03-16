d76423e9ed89d913dbceef4cda0ffd605676e30a
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2025-03-16 22:05:58 +0530

    first commit

---
+++ components/SelectPlatform.tsx
@@ -0,0 +1,68 @@
+"use client";
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useRouter } from "next/navigation";
+import { Platform } from "@/utils/data";
+
+const SelectPlatform = () => {
+  const [selectedPlatform, setSelectedPlatform] = useState("");
+  const router = useRouter();
+
+  const handleSelectPlatform = (platform: string) => {
+    setSelectedPlatform(platform);
+  };
+
+  const handleContinue = () => {
+    if (selectedPlatform) {
+      router.push(`/${selectedPlatform}`);
+    }
+  };
+
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <div className="h-full flex flex-col items-center justify-between w-full md:px-20 px-8 md:py-12 py-8">
+        <div className="grid w-full grid-cols-1 md:grid-cols-3 gap-4">
+          {Platform.map((platform, index) => (
+            <Card
+              key={index}
+              onClick={() => handleSelectPlatform(platform.href)}
+              className={`cursor-pointer md:w-[450px] w-full ${
+                selectedPlatform === platform.href ? "border-red-500" : ""
+              }`}
+            >
+              <CardHeader>
+                <CardTitle>{platform.name}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <CardDescription>
+                  Description for {platform.name}
+                </CardDescription>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+        <div className="md:flex items-center justify-between w-full mt-8">
+          <div />
+          <Button
+            disabled={selectedPlatform === ""}
+            className="bg-red-500 cursor-pointer w-full md:w-28 hover:bg-red-600"
+            onClick={handleContinue}
+          >
+            Continue <ArrowRight />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SelectPlatform;
---
+++ utils/data.ts
@@ -0,0 +1,17 @@
+export const Platform = [
+  {
+    name: "Platform 1",
+    href: "shop",
+    description: "",
+  },
+  {
+    name: "Platform 2",
+    href: "soon",
+    description: "",
+  },
+  {
+    name: "Platform 3",
+    href: "soon2",
+    description: "",
+  },
+];
